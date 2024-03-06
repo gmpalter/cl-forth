f6b6801550d00f4dffff0d556a6ded856a4ef469
Gary Palter <palter@clozure.com>
2024-03-05 23:10:46 -0500

    Add RUN-2012-TESTS

--- test.lisp
+++ test.lisp
@@ -17,6 +17,15 @@
       (let ((*standard-input* (make-concatenated-stream text *standard-input*)))
         (toplevel fs :evaluate (format nil "~@[ VERBOSE~] INCLUDE cl-forth-tests" verbose?))))))
 
+(defun run-2012-tests (&optional verbose?)
+  (write-line "CL-Forth 0.9")
+  (let ((*default-pathname-defaults* (probe-file "../forth2012-test-suite/src/"))
+        (fs (make-instance 'forth-system)))
+    (setf *fs* fs)
+    (with-input-from-string (text #.(format nil "The quick brown fox jumped over the lazy red dog.~%"))
+      (let ((*standard-input* (make-concatenated-stream text *standard-input*)))
+        (toplevel fs :evaluate (format nil "~@[ VERBOSE~] WARNING OFF S\" runtests.fth\" INCLUDED" verbose?))))))
+
 (defun hex (n)
   (format nil "~X" n))
 
