4168e7c2148451959a9c95f71e8d80781b45fc0d
Gary Palter <palter@clozure.com>
2024-03-22 16:50:07 -0400

    Add RUN-ASCENT

--- test.lisp
+++ test.lisp
@@ -23,6 +23,13 @@
 
 (setf (symbol-function 'cl-user::run-2012-tests) (symbol-function 'run-2012-tests))
 
+(defun run-ascent ()
+  (let ((fs (forth-asdf-support::fas-forth-system (asdf:find-system '#:ascent-forth))))
+    (setf *fs* fs)
+    (toplevel fs)))
+
+(setf (symbol-function 'cl-user::run-ascent) (symbol-function 'run-ascent))
+
 (defun hex (n)
   (format nil "~X" n))
 
