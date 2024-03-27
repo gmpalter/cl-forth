c1c375491c7084c70961ba194646c88faca51b26
Gary Palter <palter@clozure.com>
2024-03-26 23:27:36 -0400

    Remvoe RUN-ASCENT

--- test.lisp
+++ test.lisp
@@ -23,15 +23,6 @@
 
 (setf (symbol-function 'cl-user::run-2012-tests) (symbol-function 'run-2012-tests))
 
-(defun run-ascent ()
-  (let ((me (asdf:find-system '#:ascent-forth)))
-    (format t "~&~A ~A~%" (asdf:system-long-name me) (asdf:system-version me)))
-  (let ((fs (forth-asdf-support::fas-forth-system (asdf:find-system '#:ascent-forth))))
-    (setf *fs* fs)
-    (toplevel fs)))
-
-(setf (symbol-function 'cl-user::run-ascent) (symbol-function 'run-ascent))
-
 (defun hex (n)
   (format nil "~X" n))
 
