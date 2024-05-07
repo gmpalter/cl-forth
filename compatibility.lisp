365b92703d652f11850b27983556b940e156d876
Gary Palter <palter@clozure.com>
2024-05-07 16:28:08 -0400

    Add MAKE-PIPED-STREAMS for a tracing facility

--- compatibility.lisp
+++ compatibility.lisp
@@ -46,3 +46,57 @@
 (defun set-stream-length (stream new-length)
   (declare (ignore new-length))
   (error 'file-error :pathname (pathname stream)))
+
+
+;;; MAKE-PIPED-STREAMS
+
+#+CCL
+(defun make-piped-streams (&key (element-type 'character) (external-format :default))
+  (let* ((char-p (or (eq element-type 'character) (subtypep element-type 'character)))
+         (real-external-format (when char-p
+                                 (ccl::normalize-external-format :pipe external-format)))
+         (encoding (when char-p (ccl:external-format-character-encoding real-external-format)))
+         (line-termination (when char-p (ccl:external-format-line-termination real-external-format))))
+    (multiple-value-bind (read-fd write-fd) (ccl::pipe)
+      (let ((is (ccl::make-fd-stream read-fd
+                                     :direction :input
+                                     :interactive t
+                                     :element-type element-type
+                                     :sharing :lock
+                                     :basic t
+                                     :encoding encoding
+                                     :line-termination line-termination
+                                     :auto-close t))
+            (os (ccl::make-fd-stream write-fd
+                                     :direction :output
+                                     :interactive t
+                                     :element-type element-type
+                                     :sharing :lock
+                                     :basic t
+                                     :encoding encoding
+                                     :line-termination line-termination
+                                     :auto-close t)))
+        (ccl::add-auto-flush-stream os)
+        (values is os)))))
+
+#+SBCL
+(defun make-piped-streams (&key (element-type 'character) (external-format :default))
+  (multiple-value-bind (read-fd write-fd) (sb-unix:unix-pipe)
+    (let ((is (sb-impl::make-fd-stream read-fd
+                                       :input t
+                                       :element-type element-type
+                                       :buffering :line
+                                       :external-format external-format
+                                       :auto-close t))
+          (os (sb-impl::make-fd-stream write-fd
+                                       :output t
+                                       :element-type element-type
+                                       :buffering :line
+                                       :external-format external-format
+                                       :auto-close t)))
+      (values is os))))
+
+#+LispWorks
+(defun make-piped-streams (&key (element-type 'character) (external-format :default))
+  (declare (ignore element-type external-format))
+  (error "NYI: ~S" 'make-piped-streams))
